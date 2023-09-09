376f15b516970a3a453ee513d1e9115489ae11bc
Ng Yew Kong <ngyewkong1996@gmail.com>
2023-09-09 10:03:23 +0800

    end of section 2 ex

---
+++ hello-world/main.go
@@ -0,0 +1,11 @@
+// impt only package main will build an exe
+package main
+
+// other package names will just make it reusable code no executable will be created
+// eg. package helloworld
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
